d807be728238d2f23a65cf0ef8a9df66068c1d03
Grzegorz Skolyszewski <skolyszewski.grzegorz@gmail.com>
2021-10-25 13:05:21 +0200

    Solution #2 - passes 12/15

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"sort"
 	"strconv"
 	"strings"
 )
@@ -16,27 +17,76 @@ import (
  * The function accepts LONG_INTEGER_ARRAY price as parameter.
  */
 
+// func minimumLoss(price []int64) int32 {
+// 	// the length should be based on input's first line, but this function is
+// 	// prepared like that in the original issue, so just calculate it
+// 	pricesLen := len(price)
+// 	// then just calculate stuff
+// 	minLoss := int64(10e16)
+// 	lastPrice := price[0]
+// 	for i := 0; i < (pricesLen - 1); i++ {
+// 		currentPrice := price[i]
+// 		if currentPrice > lastPrice {
+// 			continue
+// 		}
+// 		for j := 1; j < (pricesLen - i); j++ {
+// 			newPrice := price[i+j]
+// 			if currentPrice < newPrice {
+// 				continue
+// 			}
+// 			loss := currentPrice - newPrice
+// 			if loss < minLoss {
+// 				minLoss = loss
+// 			}
+// 		}
+// 		lastPrice = currentPrice
+// 	}
+// 	return int32(minLoss)
+// }
+
 func minimumLoss(price []int64) int32 {
 	// the length should be based on input's first line, but this function is
 	// prepared like that in the original issue, so just calculate it
 	pricesLen := len(price)
-	// then just calculate stuff
-	minLoss := int64(10e16)
-	// startingPrice := 0
-	for i := 0; i < (pricesLen - 1); i++ {
-		currentPrice := price[i]
-		for j := 1; j < (pricesLen - i); j++ {
-			newPrice := price[i+j]
-			if currentPrice < newPrice {
-				continue
-			}
-			loss := currentPrice - newPrice
-			if loss < minLoss {
-				minLoss = loss
+	// // then just calculate stuff
+	minLoss := int(10e16)
+
+	intPrice := make([]int, len(price))
+	sortedIntPrice := make([]int, len(price))
+
+	for i, val := range price {
+		intPrice[i] = int(val)
+	}
+
+	copy(sortedIntPrice, intPrice)
+	sort.Ints(sortedIntPrice)
+
+	currentLen := pricesLen
+	for _, val := range intPrice {
+		sortedIndex := index(sortedIntPrice, val)
+		if sortedIndex > 0 {
+			currentMinLoss := sortedIntPrice[sortedIndex] - sortedIntPrice[sortedIndex-1]
+			if currentMinLoss < minLoss {
+				minLoss = currentMinLoss
 			}
 		}
+		sortedIntPrice = remove(sortedIntPrice, sortedIndex)
+		currentLen--
 	}
 	return int32(minLoss)
+}
+
+func index(slice []int, item int) int {
+	for i := range slice {
+		if slice[i] == item {
+			return i
+		}
+	}
+	return -1
+}
+
+func remove(slice []int, s int) []int {
+	return append(slice[:s], slice[s+1:]...)
 }
 
 func main() {
